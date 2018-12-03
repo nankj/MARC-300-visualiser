50a1654389c385dc2a6aabeb4fbb0fe57134f053
jnank <oddenda@gmail.com>
2018-12-03 16:05:41 +0000

    Refactor shelves module and change UX throughout  1) fillBay, makeShelf and makeBook functions check parameters: if a comparison the shelving area is changes and fillBay is run more than once  2) add intro modal  3) change data slider to modal. Add text to explain file upload  4) remove unused buttons  5) amend colour scheme and location of buttons  6) add summary stats

--- js/bsv.shell.js
+++ js/bsv.shell.js
@@ -11,17 +11,37 @@ bsv.shell = (function() {
 		configMap = {
 			main_html : String()
 			+	'<div class="bsv-shell-head">'
+			+		'<div id="sig">Bookshelf Visualiser</div>'
 			+		'<div class="bsv-shell-head-btn" id="btn1"></div>'
 			+		'<div class="bsv-shell-head-btn" id="btn2"></div>'
-			+		'<div class="bsv-shell-head-btn" id="btn3"></div>'
-			+		'<div class="bsv-shell-head-btn" id="btn4"></div>'
-			+		'<div class="bsv-shell-head-btn" id="btn5"></div>'
-			+		'<div class="bsv-shell-head-btn" id="btn6"></div>'			
+			+		'<div class="bsv-shell-head-btn" id="btn3">About</div>'
+			+		'<div class="bsv-shell-head-btn" id="btn4"></div>'	
 			+	'</div>'
 			+	'<div class="bsv-shell-main">'
 			+	'</div>'
-			+	'<div class="bsv-shell-data"></div>'
-			+	'<div class="bsv-shell-foot"></div>'
+			+	'<div class="bsv-shell-intro-modal">'
+			+		'<div class="bsv-shell-intro-modal-content">'
+			+		'<h2>Convert MARC data into shelves</h2>'
+			+		'<p>Large libraries have difficulty knowing how much space their stock occupies. The '
+			+		'standard cataloguing format for books includes a height measure, but does not record the '
+			+		'width. So the crucial figure of how many linear metres of shelving are occupied cannot '
+			+		'readily be calculated from the data alone. Instead, library staff have to crack out the '
+			+		'tape measure and the clipboard, and hole up for days (or weeks) in the stacks. </p>'
+			+		'<p>This tool aims to take some of the pain out of this tedious labour. It works with MARC 21, '
+			+		'using the physical description field (300|a) and other readily available metadata '
+			+		'as a proxy for the missing width value. Users can upload a CSV file, which will be '
+			+		'interpreted onscreen, providing a shelf-by shelf account of the space occupied.</p>'
+			+		'<p><b>Note:</b> In its current form this is a proof of concept. The height value '
+			+		'is missing, the width formula is '
+			+		'still very much under development, and the data collection that forms the basis of this '
+			+		'research is still ongoing. So at present (Dec \'18) any results it gives are probably wrong, '
+			+		'and certainly provisional. Watch out for developments here!</p>'
+			+	'<div class="bsv-shell-intro-modal-exampledata"></div>'
+			+	'<div class="bsv-shell-intro-modal-choosedata"></div>'		
+			+	'</div>'
+			+	'</div>'
+			+	'<div class="bsv-shell-data-modal"></div>'			
+			+	'<div class="bsv-shell-foot">Created by Joe Nankivell, &copy; 2018</div>'
 			+	'<div class="bsv-shell-start">'
 			+		'<div class="bsv-shell-start-SVG"></div>'
 			+		'<div class="bsv-shell-start-button"></div>'
@@ -30,6 +50,7 @@ bsv.shell = (function() {
 			startPageSVG : 	{
 				titleSVG	: 	{
 					viewBox	: "0 0 996 310",
+					"class"	: "titleSVG"
 					},
 				strapSVG	: 	{
 					viewBox	: "0 0 996 110",
@@ -42,8 +63,8 @@ bsv.shell = (function() {
 		},
 		jqueryMap =	{},
 		
-		makeSVG, setJqueryMap, makeStartPage,
-		onClickParams, onClickLogData, onClickStart, initModule;
+		makeSVG, setJqueryMap, makeStartPage, onClickParams, onClickLogData, 
+		onClickDataModal, onClickStart, onClickCloseIntro, initModule;
 	//----------------END MODULE SCOPE VARIABLES----------------------
 	
 	//----------------BEGIN UTILITY METHODS---------------------------
@@ -69,15 +90,21 @@ bsv.shell = (function() {
 		var $container = stateMap.$container;
 		jqueryMap = { 
 			$container 	: $container,
-			$data		: $container.find( '.bsv-shell-data'),
+			$data		: $container.find( '.bsv-shell-data-modal'),
 			$main		: $container.find( '.bsv-shell-main'),
 			$test		: $container.find( '.bsv-shell-main-test'),
 			$live		: $container.find( '.bsv-shell-main-live'),
 			$start		: $container.find( '.bsv-shell-start'),
 			$startSVG	: $container.find( '.bsv-shell-start-SVG'),
-			$startbtn	: $container.find( '.bsv-shell-start-button'),			
+			$startbtn	: $container.find( '.bsv-shell-start-button'),
+			$intro		: $container.find( '.bsv-shell-intro-modal'),
+			$introbtn1	: $container.find( '.bsv-shell-intro-modal-exampledata'),
+			$introbtn2	: $container.find( '.bsv-shell-intro-modal-choosedata'),
+			$headbtns	: $container.find( '.bsv-shell-head-btn'),
+			$btn1		: $container.find( '#btn1' ),					
 			$btn2		: $container.find( '#btn2'),
 			$btn3		: $container.find( '#btn3'),
+			$btn4		: $container.find( '#btn4'),
 		};
 	};
 	// End DOM method /setJqueryMap/	
@@ -100,39 +127,22 @@ bsv.shell = (function() {
 				"dominant-baseline"	: "middle",
 				"class"				: "strapline",	
 			})
-/*			startButton = makeSVG("rect", {
-				x					: "44%",
-				y					: "40%",
-				width				: "12%",
-				height				: "20%",
-				rx					: "8",
-				ry					: "8",				
-				fill				: "#F2F1EF",
-				"class"				: "bsv-shell-start-button"
-			}),
-			startButtonText 	= makeSVG("text", {
-				x					: "50%",
-				y					: "50%",
-				"text-anchor"		: "middle",
-				"dominant-baseline"	: "middle",				
-				fill				: "red",
-				"class"				: "bsv-shell-start-button-text"
-			})
-*/			;
+			;
+		
+		bsv.data.setParam("is_comparison", false);
 			
 		title.innerHTML = "Bookshelf Visualiser";
 		strapLine.innerHTML = "Measure your stock without leaving your desk";
-//		startButtonText.innerHTML = "Start";
-		
+
+// WILL NEED TO AMEND TO FIX DIV LOCATION 25-11-18
 		div.append(shelfBox);	
-		bsv.shelves.fillBay(data, 1, 0, shelfBox, true, true);
+		
+		bsv.shelves.fillBay(data, 1, 0, bsv.data.getAllParams(), true);
 		shelfBox.appendChild(title);	
 
 		strapBox.appendChild(strapLine);
-//		strapBox.appendChild(startButton);
-//		strapBox.appendChild(startButtonText);
 		div.append(strapBox);	
-		return true;
+		return bsv.data.setParam("is_comparison", true);;
 	};
 	
 	//----------------END DOM METHODS---------------------------------
@@ -140,7 +150,7 @@ bsv.shell = (function() {
 	//----------------BEGIN EVENT HANDLERS----------------------------
 	
 	onClickParams = function ( event ) {
-		console.log( bsv.data.getParams('ppmm') );
+		console.log( bsv.data.getAllParams() );
 		return false;
 	};
 	
@@ -148,10 +158,24 @@ bsv.shell = (function() {
 		console.table( bsv.data.getData() );
 		return false;
 	};
+
+	onClickDataModal = function ( event ) {
+		jqueryMap.$intro.addClass('bsv-x-clearfloat')
+		jqueryMap.$data.toggleClass('bsv-x-clearfloat');
+		jqueryMap.$headbtns.removeClass('bsv-x-clearfloat');		
+		return false;
+	};
 	
 	onClickStart = function ( event ) {
 		jqueryMap.$start.empty();
 		jqueryMap.$start.addClass('bsv-x-clearfloat');
+		return false;
+	};	
+
+	onClickCloseIntro = function ( event ) {
+		jqueryMap.$intro.empty();
+		jqueryMap.$intro.addClass('bsv-x-clearfloat');
+		jqueryMap.$headbtns.removeClass('bsv-x-clearfloat');
 		return false;
 	};	
 	//----------------END EVENT HANDLERS------------------------------
@@ -164,11 +188,7 @@ bsv.shell = (function() {
 		$container.html( configMap.main_html );
 		setJqueryMap();
 				
-
-		// set view to live tool
-		jqueryMap.$test
-			.addClass('bsv-x-clearfloat');
-			
+		
 		// initialise data and shelves modules
 		bsv.data.initModule(jqueryMap.$data);
 		bsv.shelves.initModule(jqueryMap.$main);
@@ -176,28 +196,48 @@ bsv.shell = (function() {
 		// Load start page SVG
 		makeStartPage(jqueryMap.$startSVG);
 
-		// initialise buttons and bind click handlers
+		// Hide data modal and header buttons
+		jqueryMap.$data.addClass('bsv-x-clearfloat');
+		jqueryMap.$headbtns.addClass('bsv-x-clearfloat');	
 
-		jqueryMap.$btn2
-			.text('Params')
-			.attr('title', 'Log current parameters')
-			.click( onClickParams );
+		// initialise buttons and bind click handlers
 		
-		jqueryMap.$btn3
-			.text('Data')
+		jqueryMap.$btn1
+			.text('')
 			.attr('title', 'Log current book data')
 			.click( onClickLogData );
+
+		jqueryMap.$btn2
+			.text('')
+			.attr('title', 'Log current parameters')
+			.click( onClickParams );
+
+		jqueryMap.$btn4
+			.text('Change data')
+			.attr('title', 'Change data source')
+			.click( onClickDataModal );
 		
 		jqueryMap.$startbtn
 			.text('START')
-			.attr('title', 'Click to turn your data into bookshelves')
+			.attr('title', 'Turn your data into bookshelves')
 			.click( onClickStart );
+
+		jqueryMap.$introbtn1
+			.text('Use example')
+			.attr('title', 'Use example data')
+			.click( onClickCloseIntro );
+
+		jqueryMap.$introbtn2
+			.text('Select data')
+			.attr('title', 'Select a data source')
+			.click( onClickDataModal );
 			
-
-
 	};
 	// End Public method /initModule/
 	
-	return { initModule : initModule };
+
+	return { 
+		initModule 	: initModule
+	};
 	//----------------END PUBLIC METHODS------------------------------	
 }());
